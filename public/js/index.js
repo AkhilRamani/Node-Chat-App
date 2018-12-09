fa00cac243692d993e188a6032c25ed75cfa038f
Akhil <ramaniakhil094@gmail.com>
2018-12-09 21:47:39 +0530

    Added auto scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket= io();
 
+function scrollToBottom(){
+    var messages= jQuery('#messages');
+    var newMessage= messages.children('li:last-Child');
+
+    var clientHeight= messages.prop('clientHeight');
+    var scrollTop= messages.prop('scrollTop');
+    var scrollHeight= messages.prop('scrollHeight');
+    var newMessageHeight= newMessage.innerHeight();
+    var lastMessageHeight= newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function() {
     console.log('Connected to the server');
 });
@@ -18,6 +33,7 @@ socket.on('newMessage', function(message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message){
@@ -31,6 +47,7 @@ socket.on('newLocationMessage', function(message){
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(e){
